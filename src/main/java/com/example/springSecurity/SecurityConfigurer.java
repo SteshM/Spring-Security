196e040c399bec719ee41d93543931cc5970bff4
SteshM <mbithestella0@gmail.com>
2023-06-22 12:06:56 +0300

    created authorization filter

--- src/main/java/com/example/springSecurity/SecurityConfigurer.java
+++ src/main/java/com/example/springSecurity/SecurityConfigurer.java
@@ -2,12 +2,15 @@ package com.example.springSecurity;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
@@ -20,5 +23,15 @@ public class SecurityConfigurer {
     UserDetailsService userDetailsService;
     public void configurer(AuthenticationManagerBuilder auth) throws Exception{
         auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+    }
+    @Bean
+    public SecurityFilterChain configurer(HttpSecurity http) throws Exception{
+        return http
+                .csrf((csrf)-> csrf.disable())
+                .authorizeHttpRequests((authorizeHttpRequests)->authorizeHttpRequests.requestMatchers(("/login")).permitAll())
+                .authorizeHttpRequests((authorizeHttpRequests)->authorizeHttpRequests.requestMatchers(("/Welcome")).hasAnyAuthority("admin"))
+                .authorizeHttpRequests((authorizeHttpRequests)->authorizeHttpRequests.requestMatchers(("/Welcome")).hasAnyAuthority("Student"))
+                .authorizeHttpRequests((authorizeHttpRequests)->authorizeHttpRequests.requestMatchers(("/Welcome")).hasAnyAuthority("Lecturer"))
+                .authorizeHttpRequests((authorizeHttpRequests)->authorizeHttpRequests.anyRequest().authenticated())
     }
 }
